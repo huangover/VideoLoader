4e302e5e784590c8943c91c4637661e855cb593d
Sihang Huang <sihang@sihangs-macbook-pro.local>
2018-10-22 20:23:47 +0800

    Initial Commit

---
+++ VideoLoader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoLoader
+//
+//  Created by Sihang Huang on 10/22/18.
+//  Copyright © 2018 Sihang Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
